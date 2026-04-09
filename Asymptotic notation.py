c8265f632be2c0037dfc4ab810a4fb98d298fcb3
Rayyan <Rayyanshafqat@gmail.com>
2026-04-09 22:33:28 +0500

    initial

---
+++ Asymptotic notation.py
@@ -0,0 +1,9 @@
+def printnumber(n):
+    iteration=0
+    print("The number entered by the user is:",n)
+    iteration+=1
+    print("Total iteration done in the code is",iteration,"/n")
+
+printnumber(10)
+printnumber(20)
+print("/n With any 'n' the time taken by our code would not change.")    
