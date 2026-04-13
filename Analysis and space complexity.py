aa9d7d7cdd39d843c6d1353cfa1cc86a85e8c079
Rayyan <Rayyanshafqat@gmail.com>
2026-04-13 22:33:41 +0500

    initial

---
+++ Analysis and space complexity.py
@@ -0,0 +1,9 @@
+def prints(n):
+    if n <=0:
+      return
+    print('Hello')
+    print( n // 2)
+    print( n // 2)
+
+#Call funtion     
+print(8)
---
+++ some space complexity.py
@@ -0,0 +1,3 @@
+def sum_n(n):
+   return n * (n+1) // 2
+print("sum of the some n numbers (n = 5)",sum_n(5))
